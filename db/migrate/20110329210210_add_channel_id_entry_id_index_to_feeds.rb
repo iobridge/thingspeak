e16aa9fe9c5722a5132f644a7d0953be220bddd2
ioBridge <support@iobridge.com>
2011-03-29 19:13:14 -0400

    fixing database tables

--- db/migrate/20110329210210_add_channel_id_entry_id_index_to_feeds.rb
+++
@@ -1,9 +0,0 @@
-class AddChannelIdEntryIdIndexToFeeds < ActiveRecord::Migration
-  def self.up
-		add_index :feeds, [:channel_id, :entry_id]
-  end
-
-  def self.down
-		remove_index :feeds, [:channel_id, :entry_id]
-  end
-end

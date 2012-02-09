bfd5b29a9a9cfae4db335cf34d5ee6ca7d6b4655
Alan Bradburne <abradburne@gmail.com>
2012-02-09 23:26:47 +0000

    Upgrade to Rails 3.2 and move some of the assets to the asset pipeline directories

--- Gemfile
+++ Gemfile
@@ -1,8 +1,9 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.4' 
+gem 'rails', '3.2.1'
 gem 'mysql2'
 gem 'authlogic'
+gem 'jquery-rails'
 
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,11 @@ Thingspeak::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
+  config.assets.allow_debugging = true
+
+  # Configure static asset server for tests with Cache-Control for performance
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
 end

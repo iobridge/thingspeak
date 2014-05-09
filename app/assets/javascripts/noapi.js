ab7694dae9b1df644a1ea6e72acf1bdd13ddd114
Lee Lawlor <primemod3@gmail.com>
2014-05-09 13:32:52 -0400

    fix api redirect for public plugins

--- app/assets/javascripts/noapi.js
+++ app/assets/javascripts/noapi.js
@@ -1,6 +1,6 @@
-// if on api subdomain except for charts, redirect
+// if on api subdomain except for charts and plugins, redirect
 var wloc = window.location.toString();
-if (wloc.indexOf('api') !== -1 && wloc.indexOf('api') < 10 && wloc.indexOf('charts') === -1) {
+if (wloc.indexOf('api') !== -1 && wloc.indexOf('api') < 10 && wloc.indexOf('charts') === -1 && wloc.indexOf('plugins') === -1) {
   window.location = wloc.replace('api', 'www');
 }
 

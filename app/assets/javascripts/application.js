cb7fedfb3be34f5677001a6b639c681510e2b540
Lee Lawlor <primemod3@gmail.com>
2014-05-12 17:01:14 -0400

    update channels index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,7 @@
 //= require ./updateChart.js
 //= require ./timeago.min.js
 //= require ./jquery.shorten.min.js
+//= require ./tablesorter.js
 //= require ./nested_form.js
 //= require ./noapi.js
 //= require ./rest.js

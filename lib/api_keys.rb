9606889aac2698421effd8f121b4ea05b992675c
Lee Lawlor <primemod3@gmail.com>
2014-06-29 16:21:27 -0400

    fix error when user doesn't own channel and tries to view api keys

--- lib/api_keys.rb
+++ lib/api_keys.rb
@@ -1,11 +1,12 @@
 module ApiKeys
-  def api_index (channel_id)
+  def api_index(channel_id)
     if current_user && !current_user.channels.find_by_id(channel_id).nil?
       @channel = current_user.channels.find(channel_id)
     end
-    if current_user && current_user.id == @channel.user_id
+    if current_user.present? && @channel.present? && current_user.id == @channel.user_id
       @write_key = @channel.api_keys.write_keys.first
       @read_keys = @channel.api_keys.read_keys
     end
   end
 end
+

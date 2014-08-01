0c758f6e4042ef843ba52c43219f1effe6eeddb5
Lee Lawlor <primemod3@gmail.com>
2014-08-01 15:57:02 -0400

    turbolinks fixes

--- app/assets/javascripts/channels.js
+++ app/assets/javascripts/channels.js
@@ -1,11 +1,7 @@
-$(function () {
-      $("div.progressbar").each (
-	  function () {
-	      var element = this;
-	      $(element).progressbar(
-		  {
-		      value: parseInt($(element).attr("rel"))
-		  });
-	  });
+$(document).on('page:load ready', function() {
+  $("div.progressbar").each(function() {
+    var element = this;
+    $(element).progressbar({ value: parseInt($(element).attr("rel")) });
+  });
 });
 
